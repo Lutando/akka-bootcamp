36914d1b60d74484e74a1d47a657704abe0e1308
Lutando Ngqakaza <lutando@ngqakaza.com>
2016-11-24 14:58:32 +0200

    lesson 3 done...

---
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class ValidationActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+
+
+        public ValidationActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                _consoleWriterActor.Tell(new NullInputError("No input received."));
+            }
+            else
+            {
+                var valid = IsValid(msg);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new InputSuccess("Thank you! Message was valid."));
+
+                }
+                else
+                {
+                    _consoleWriterActor.Tell(new ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+
+            // tell sender to continue doing its thing
+            // (whatever that may be, this actor doesn't care)
+            Sender.Tell(new ContinueProcessing());
+
+        }
+
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Currently says messages are valid if contain even number of characters.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
+    }
+}
